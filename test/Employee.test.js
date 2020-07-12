2ac4173f1434f095e039a52d6389097c0453bca7
John Kim <johnk9000@gmail.com>
2020-07-12 16:45:51 -0500

    Succesfully tested Employee class constructor

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -11,7 +11,7 @@ describe("Employee", () => {
             expect(employee.role).toEqual('engineer');
         });
 
-        it("should throw an error if provided no arguements", () => {
+        it("should throw an error if provided no arguments", () => {
             const cb = () => Employee();
 
             expect(cb).toThrow();
@@ -19,9 +19,65 @@ describe("Employee", () => {
 
         it("should throw an error if 'name' is not provided", () => {
             const cb = () => new Employee();
-            const err = new Error("Expect parameter 'name' to be a non-empty string");
+            const err = new Error("Expected parameter 'name' to be a non-empty string");
 
             expect(cb).toThrowError(err);
+        });
+
+        it("should throw an error if 'name' is not a string", () => {
+            const cb = () => new Employee(420, 420);
+            const err = new Error("Expected parameter 'name' to be a non-empty string");
+      
+            expect(cb).toThrowError(err);
+          });
+
+        it("should throw an error if 'id' is not provided", () => {
+            const cb = () => new Employee('John');
+            const err = new Error("Expected parameter 'id' to be a non-negative number");
+
+            expect(cb).toThrowError(err);
+        });
+
+        it("should throw an error if 'id' is not a number", () => {
+            const cb = () => new Employee("John", "string");
+            const err = new Error("Expected parameter 'id' to be a non-negative number");
+        
+            expect(cb).toThrowError(err);
+        });
+
+        it("should throw an error if 'email' is not provided", () => {
+            const cb = () => new Employee('John', 420);
+            const err = new Error("Expected parameter 'email' to be a non-empty string");
+
+            expect(cb).toThrowError(err);
+        });
+
+        it("should throw an error if 'email' is not a string", () => {
+            const cb = () => new Employee('John', 420, 420);
+            const err = new Error("Expected parameter 'email' to be a non-empty string");
+      
+            expect(cb).toThrowError(err);
+          });
+
+        it("should throw an error if 'role' is not provided", () => {
+            const cb = () => new Employee('John', 420, 'johnk9000@gmail.com');
+            const err = new Error("Expected parameter 'role' to be a non-empty string");
+
+            expect(cb).toThrowError(err);
+        });
+
+        it("should throw an error if 'role' is not a string", () => {
+            const cb = () => new Employee('John', 420, 'johnk9000@gmail.com', 420);
+            const err = new Error("Expected parameter 'role' to be a non-empty string");
+      
+            expect(cb).toThrowError(err);
+        });
+
+        it("should throw an error if 'role' is not either 'Intern','Manager', or 'Engineer'", () => {
+        const cb = () => new Employee('John', 420, 'johnk9000@gmail.com', 'wizard');
+        const err = new Error("Invalid role was specified");
+
+        expect(cb).toThrowError(err);
         });
     })
 })
