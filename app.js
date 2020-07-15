4b8036020216c13dae78d4187d80d49668d19218
John Kim <johnk9000@gmail.com>
2020-07-14 23:40:59 -0500

    Struggling but I am at the last stretch
    I will be late, I am sorry.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
 const util = require("util");
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const render = require("./lib/htmlRenderer");
+const renderPage = require("./lib/htmlRenderer");
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
+const readFileAsync = util.promisify(fs.readFile);
+let count = 0;
+
 var questions = [
     {
         type: "input",
@@ -27,23 +35,24 @@ var questions = [
     `  
     },
     {
-        type: "checkbox",
+        type: "list",
         name: "role",
         message:`
         Choose Role`,
         choices: [
             "Intern",
             "Manager",
-            "Engineer"
+            "Engineer",
         ]
     },
 ];
-
+// The main prompt which will ask users for name, id, e-mail, and their role
 function prompt()  {
-
+    count++;
+        console.log('Starting new profile');
     return inquirer.prompt(questions);
 }
-
+// One of the folloiwing three prompts will be called in init *after* usr 'role' is defined
 function promptEngineer() {
     return inquirer.prompt({ 
         type: 'input',
@@ -74,33 +83,90 @@ What school did you graduate from most recently?
     })
 }
 
+let employees = [];
 
 async function init() {
-    try {
-        const test = await prompt();
-        const role = test.role.toString();
-        var persona = new Employee(test.name, parseInt(test.id), test.email, role);
+    let len = employees.length;
+    let order = await startCommand();
+    order = order.file.toString();
+        //console.log(order);
+    if(order === "Create a Profile") {
+        try {
+            const test = await prompt();
+            const role = test.role.toString();
+            var persona = new Employee(test.name, parseInt(test.id), test.email, role);
+            let fileName = 'cache.txt'
+            // Role specific prompts and construction of one of three class extensions
+            if(role === 'Engineer') {
+                const username = await promptEngineer()
+                var engineer = new Engineer(test.name, parseInt(test.id), test.email, role, username.gitHub)
+                        //engineer = JSON.stringify(engineer);
+                        console.log("Engineer's profile data: " + engineer);
+                    employees.push(engineer)
+                    if(len < employees.length) {
+                        init()
+                    }
 
-        if(role === 'Engineer') {
-            const username = await promptEngineer();
-            var engineer = new Engineer(test.name, parseInt(test.id), test.email, role, username.gitHub)
-                console.log("Engineer's profile data: " + JSON.stringify(engineer));
-        } else if(role === 'Manager') {
-            const office = await promptManager();
-            var manager = new Manager(test.name, parseInt(test.id), test.email, role, office.officeNum)
-                console.log("Manager's profile data: " + manager.toString());
-        } else if(role === 'Intern') {
-            const uni = await promptIntern();
-            var intern = new Intern(test.name, parseInt(test.id), test.email, role, uni.school)
-                console.log("Intern's profile data: " + intern.toString());
-        } else {
-            console.log('error')
+            } else if(role === 'Manager') {
+                const office = await promptManager()
+                var manager = new Manager(test.name, parseInt(test.id), test.email, role, office.officeNum)
+                        //manager = JSON.stringify(manager);
+                        console.log("Manager's profile data: " + manager);
+                    employees.push(manager)
+                    if(len < employees.length) {
+                        init()
+                    }
+
+
+            } else if(role === 'Intern') {
+                const uni = await promptIntern()
+                var intern = new Intern(test.name, parseInt(test.id), test.email, role, uni.school)
+                        //intern = JSON.stringify(intern);
+                        //console.log("Intern's profile data: " + intern);
+                    employees.push(intern)
+                        console.log("employees: " + employees.length);
+                        console.log(len)
+                        if(len < employees.length) {
+                            init();
+                        }
+    
+
+            } else {
+                console.log('error')
+
+            }
+        } catch(err) {
+            console.log(err)
         }
-    } catch(err) {
-        console.log(err)
+        
+    } else if (order === "Clear Cache") {
+        await writeFileAsync('cache.txt', "");
+    } else if (order == "Save All & Exit"){
+        employees = JSON.stringify(employees);
+        console.log(employees)
+        await appendFileAsync('cache.txt', employees);
+        await renderPage();
+        //RENDER HTML HERE
+        console.log('Exiting...')
     }
-    console.log('writing answers')
+
 }
 
+function startCommand() {
+    return inquirer.prompt({
+        type: 'list',
+        message: new inquirer.Separator(),
+        name: 'file',
+        choices: [
+            "Create a Profile",
+            "Save All & Exit",
+            new inquirer.Separator(),
+            "Clear Cache",
+            new inquirer.Separator(),
+    ]
+    })
+}
 
 init();
+// init Fcn that will asyncronously run prompts and await answers before kicking off the next series of prompts
+//module.exports = init();
